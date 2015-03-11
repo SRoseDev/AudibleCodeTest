07224d835a43842356b9f8a7f4cb22c94dad1372
Scott Rose <s.rose.dev.88@gmail.com>
2015-03-11 17:44:52 -0400

    Added Drag and Drop Functionality

--- app/src/main/java/com/dev_4wardwv/android/audiblecodetest/CustomRecyclerAdapter.java
+++ app/src/main/java/com/dev_4wardwv/android/audiblecodetest/CustomRecyclerAdapter.java
@@ -1,7 +1,11 @@
 package com.dev_4wardwv.android.audiblecodetest;
 
+import android.content.ClipData;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -14,7 +18,7 @@ import java.util.ArrayList;
  * Created by Android1 on 2/25/2015.
  */
 public class CustomRecyclerAdapter
-        extends RecyclerView.Adapter<RecyclerViewHolder> implements View.OnClickListener{
+        extends RecyclerView.Adapter<RecyclerViewHolder> implements View.OnClickListener {
 
     private ArrayList<Data> mData = new ArrayList<>();
 
@@ -39,7 +43,7 @@ public class CustomRecyclerAdapter
         return new RecyclerViewHolder(itemView);
     }
 
-    public void onClick(View V){
+    public void onClick(View V) {
 
         ImageView clicked = (ImageView) V;
 
@@ -73,10 +77,16 @@ public class CustomRecyclerAdapter
 
         viewHolder.largeImage.setOnClickListener(this);
         viewHolder.largeImage.setTag(mData.get(position).largeImg);
+        viewHolder.largeImage.setOnTouchListener(new MyTouchListener());
+        viewHolder.largeImage.setOnDragListener(new MyDragListener());
         viewHolder.topImage.setOnClickListener(this);
         viewHolder.topImage.setTag(mData.get(position).topImg);
+        viewHolder.topImage.setOnTouchListener(new MyTouchListener());
+        viewHolder.topImage.setOnDragListener(new MyDragListener());
         viewHolder.bottomImage.setOnClickListener(this);
         viewHolder.bottomImage.setTag(mData.get(position).bottomImg);
+        viewHolder.bottomImage.setOnTouchListener(new MyTouchListener());
+        viewHolder.bottomImage.setOnDragListener(new MyDragListener());
 
         //stove values in cache based on current drawable id and original url
         //MainActivity.urlCache.put(mData.get(position).largeImg, mData.get(position).largeImg);
@@ -84,6 +94,138 @@ public class CustomRecyclerAdapter
         //MainActivity.urlCache.put(String.valueOf(viewHolder.bottomImage.getId()), mData.get(position).bottomImg);
 
     }
+
+    private float mDownX;
+    private float mDownY;
+    private final float SCROLL_THRESHOLD = 10;
+    private boolean isOnClick;
+
+    // This defines your touch listener
+    private final class MyTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent ev) {
+
+            switch (ev.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    mDownX = ev.getX();
+                    mDownY = ev.getY();
+                    isOnClick = true;
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                    view.setAlpha(1);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (isOnClick) {
+                        //Log.i(LOG_TAG, "onClick ");
+                        //TODO onClick code
+                    }
+                    view.setAlpha(1);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (isOnClick && (Math.abs(mDownX - ev.getX()) > SCROLL_THRESHOLD || Math.abs(mDownY - ev.getY()) > SCROLL_THRESHOLD)) {
+                        // Log.i(LOG_TAG, "movement detected");
+                        isOnClick = false;
+                        ClipData data = ClipData.newPlainText("", "");
+                        View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                        view.startDrag(data, shadowBuilder, view, 0);
+                       // view.setVisibility(View.INVISIBLE);
+                        view.setAlpha(.5f);
+                        return true;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            return false;
+        }
+    }
+
+    class MyDragListener implements View.OnDragListener {
+        //Drawable enterShape = getResources().getDrawable(R.drawable.shape_droptarget);
+        //Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //v.setBackgroundDrawable(enterShape);
+                    v.setBackgroundColor(Color.CYAN);
+                    v.setPadding(2,2,2,2);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //v.setBackgroundDrawable(normalShape);
+                    v.setBackgroundColor(Color.TRANSPARENT);
+                    v.setPadding(0,0,0,0);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    v.setBackgroundColor(Color.TRANSPARENT);
+                    v.setPadding(0,0,0,0);
+                    // Dropped, swap images
+                    View view = (View) event.getLocalState();
+                    ImageView container = (ImageView) v;
+                    //container.set(view);
+                    String sourceUrl = view.getTag().toString();
+                    String containerUrl = container.getTag().toString();
+                    int sourceSize = view.getHeight();
+                    int containerSize = container.getHeight();
+                    //Swap Images
+                    if(containerSize > 200) {
+                        Picasso.with(MainActivity.context)
+                                .load(sourceUrl)
+                                .resize(310, 310)
+                                .centerCrop()
+                                .into(container);
+                        //view.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        Picasso.with(MainActivity.context)
+                                .load(sourceUrl)
+                                .resize(150, 150)
+                                .centerCrop()
+                                .into(container);
+                        //view.setVisibility(View.VISIBLE);
+                    }
+
+                    if(sourceSize > 200) {
+                        Picasso.with(MainActivity.context)
+                                .load(containerUrl)
+                                .resize(310, 310)
+                                .centerCrop()
+                                .into((ImageView)view);
+                        //view.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        Picasso.with(MainActivity.context)
+                                .load(containerUrl)
+                                .resize(150, 150)
+                                .centerCrop()
+                                .into((ImageView)view);
+                        //view.setVisibility(View.VISIBLE);
+                    }
+
+                    //swap tags to account for url change
+                    container.setTag(sourceUrl);
+                    view.setTag(containerUrl);
+
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //v.setBackgroundDrawable(normalShape);
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+
+
+
+
 
     public void addItem(int position, Data data) {
 
